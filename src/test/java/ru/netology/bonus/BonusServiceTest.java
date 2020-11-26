234b62b914911320470d2a052dbad55e5706f021
Cliffart <Cliffart44@gmail.com>
2020-11-26 17:57:32 +0300

    Custom delimiter added

--- src/test/java/ru/netology/bonus/BonusServiceTest.java
+++ src/test/java/ru/netology/bonus/BonusServiceTest.java
@@ -2,13 +2,12 @@ package ru.netology.bonus;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class BonusServiceTest {
     @ParameterizedTest
-    @CsvFileSource(resources = "/data.csv")
+    @CsvFileSource(resources = "/data.csv", delimiter ='|')
     void shouldCalculate(String test, long amount, boolean registered, long expected) {
         BonusService service = new BonusService();
 
